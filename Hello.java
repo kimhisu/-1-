f298a3eefa2231c897085f37245341af6d8280d0
kimhisu <163235021+kimhisu@users.noreply.github.com>
2024-03-13 21:35:27 +0900

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 package school;
 
 public class Hello {
-
+public static void main(String[] args) {
+	System.out.println("안녕!");
+	System.out.println("안녕" + "!");
+	
+	String hello = "안녕!";
+	System.out.println(hello);
+	
+}
 }
