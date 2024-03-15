1c9d39814e123c6fac14ae3a72674691c5c2fe1d
kimhisu <163235021+kimhisu@users.noreply.github.com>
2024-03-15 10:03:29 +0900

    Add files via upload

---
+++ 원기둥부피.java
@@ -0,0 +1,19 @@
+package school;
+
+public class 원기둥부피 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int radius;
+int height;
+
+radius = 10;
+height = 10;
+double volume;
+volume = radius * radius * 3.14 * height;
+
+System.out.println("원기둥이 부피는" + volume);
+
+	}
+
+}
