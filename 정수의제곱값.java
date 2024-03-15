15fd4d6b710ab8c087f395c015b15b86eee5ab6d
kimhisu <163235021+kimhisu@users.noreply.github.com>
2024-03-15 10:03:42 +0900

    Add files via upload

---
+++ 정수의제곱값.java
@@ -0,0 +1,13 @@
+package school;
+
+public class 정수의제곱값 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int seven = 7;
+int squared = 49;
+
+System.out.println(seven + "의 제곱은" + squared);
+	}
+
+}
