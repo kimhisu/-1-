0b925cf5a34c1e58a0b9d7119d27d236ec7d5304
kimhisu <163235021+kimhisu@users.noreply.github.com>
2024-03-20 15:36:17 +0900

    Add files via upload

---
+++ BreakDemo.java
@@ -0,0 +1,16 @@
+package src12;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 1, j  = 5;
+
+while (true) {
+	System.out.print(i++);
+	if (i >= j)
+		break;
+}
+	}
+
+}
